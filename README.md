439ed1448a0da57122e05b12c29d4f315445c620
Mark Silverberg <mark@marksilver.net>
2016-10-05 17:09:37 -0700

    added conversions to RAML and Swagger

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # domino-api-docs
-## Unofficial (for now), Domino Data Lab API resources
 
-Documented using [API Blueprint](https://apiblueprint.org) and rendered using [aglio](https://github.com/danielgtaylor/aglio)
+Unofficial _(for now)_, Domino Data Lab API resources
+
+### Input
+* `apiary.apib` is our API documented using the [API Blueprint](https://apiblueprint.org) standard and Apiary editor
+
+### Output
+* `index.html` docs converted by [aglio](https://github.com/danielgtaylor/aglio)
+* `swagger_2.0.yaml` is Swagger 2.0 YAML converted by [apitransformer.com](https://apitransformer.com)
+* `raml_0.8.yaml` is RAML 0.8 YAML converted by [apitransformer.com](https://apitransformer.com)
--- convert.sh
+++ convert.sh
@@ -1,5 +1,29 @@
+echo "Converting apiary.apib to index.html using aglio"
 aglio \
 	--theme-variables default \
 	-i apiary.apib \
 	-o index.html \
 	--theme-template triple
+
+
+echo "Converting apiary.apib to Swagger 2.0 YAML using apitransformer.com"
+curl \
+  -F "file=@apiary.apib" \
+  https://apitransformer.com/api/transform?output=swagger20yaml \
+  > swagger_2.0.yaml
+
+sleep 5 
+
+echo "Converting apiary.apib to Swagger 2.0 JSON using apitransformer.com"
+curl \
+  -F "file=@apiary.apib" \
+  https://apitransformer.com/api/transform?output=swagger20json \
+  > swagger_2.0.json
+
+sleep 5 
+
+echo "Converting apiary.apib to RAML 0.8 YAML using apitransformer.com"
+curl \
+  -F "file=@apiary.apib" \
+  https://apitransformer.com/api/transform?output=raml08 \
+  > raml_0.8.yaml
