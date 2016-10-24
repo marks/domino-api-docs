c22a833fc9479acf66cc4d5ae12be6dfb3374627
Mark Silverberg <mark@marksilver.net>
2016-10-23 18:19:15 -0700

    turns out, apitransformer.com wasnt doing much

--- convert.sh
+++ convert.sh
@@ -6,24 +6,24 @@ aglio \
 	--theme-template triple
 
 
-echo "Converting apiary.apib to Swagger 2.0 YAML using apitransformer.com"
-curl \
-  -F "file=@apiary.apib" \
-  https://apitransformer.com/api/transform?output=swagger20yaml \
-  > swagger_2.0.yaml
+# echo "Converting apiary.apib to Swagger 2.0 YAML using apitransformer.com"
+# curl \
+#   -F "file=@apiary.apib" \
+#   https://apitransformer.com/api/transform?output=swagger20yaml \
+#   > swagger_2.0.yaml
 
-sleep 5 
+# sleep 5 
 
-echo "Converting apiary.apib to Swagger 2.0 JSON using apitransformer.com"
-curl \
-  -F "file=@apiary.apib" \
-  https://apitransformer.com/api/transform?output=swagger20json \
-  > swagger_2.0.json
+# echo "Converting apiary.apib to Swagger 2.0 JSON using apitransformer.com"
+# curl \
+#   -F "file=@apiary.apib" \
+#   https://apitransformer.com/api/transform?output=swagger20json \
+#   > swagger_2.0.json
 
-sleep 5 
+# sleep 5 
 
-echo "Converting apiary.apib to RAML 0.8 YAML using apitransformer.com"
-curl \
-  -F "file=@apiary.apib" \
-  https://apitransformer.com/api/transform?output=raml08 \
-  > raml_0.8.yaml
+# echo "Converting apiary.apib to RAML 0.8 YAML using apitransformer.com"
+# curl \
+#   -F "file=@apiary.apib" \
+#   https://apitransformer.com/api/transform?output=raml08 \
+#   > raml_0.8.yaml
